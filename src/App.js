5489d34ec40de2c257620682a5edf967a6773a02
CihatAld <cihataldanmaz@gmail.com>
2020-03-17 21:52:49 -0400

    players added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
-import React from 'react';
-import './App.css';
-import { Container, Col, Row } from 'reactstrap';
-import PlayerList from './PlayerList';
+import React, { Component } from 'react'
+import PlayerList from "./PlayerList"
+import Field from "./Field"
 import './style/style.css'
-import Field from './Field';
 
-
-function App() {
-
-  let jsonexample = () => {
-    let players = {
-      players1:{
-        id:1,
-        name:"muslera",
-        number:1,
-        position:"GK"
-      }
-    }
-  }
-
-  return (
-    <div className = "maindiv">
+export default class App extends Component {
+  render() {
+    return (
+      <div className = "maindiv">
      
           <div className = "playerscomponent">
-        <PlayerList  players = {this.jsonexample}/>
+        <PlayerList/>
         </div>
         <div className = "fieldcomponent">
       <Field />
         </div>
 
     </div>
-  );
+    )
+  }
 }
-
-export default App;
